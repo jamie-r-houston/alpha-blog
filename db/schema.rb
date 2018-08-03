8c11d9f7b1273c11f91d5405da76cbd2e481aead
Jamie Houston <jamierhouston@hotmail.com>
2018-08-03 21:01:03 +0100

    user article meta data added to articles

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_08_03_184547) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "create_at"
+    t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
   end
